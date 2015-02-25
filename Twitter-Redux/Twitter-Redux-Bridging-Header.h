6d5be53de295cd6be8a7b71c874e2a217f5c0a39
Rachel Thomas <racheltho@gmail.com>
2015-02-24 20:46:01 -0800

    duplicated and renamed Twitter-App, after much suffering

---
+++ Twitter-Redux/Twitter-Redux-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Twitter-Redux-Bridging-Header.h
+//  Twitter-Redux
+//
+//  Created by Rachel Thomas on 2/24/15.
+//  Copyright (c) 2015 Rachel Thomas. All rights reserved.
+//
+
+#ifndef Twitter_Redux_Twitter_Redux_Bridging_Header_h
+#define Twitter_Redux_Twitter_Redux_Bridging_Header_h
+
+#import <UIImageView+AFNetworking.h>
+#import <SVProgressHUD.h>
+#import <BDBOAuth1RequestOperationManager.h>
+
+#endif
--- Twitter-Redux/dummy.m
+++ Twitter-Redux/dummy.m
@@ -1,8 +1,8 @@
 //
 //  dummy.m
-//  Twitter-App
+//  Twitter-Redux
 //
-//  Created by Rachel Thomas on 2/18/15.
+//  Created by Rachel Thomas on 2/24/15.
 //  Copyright (c) 2015 Rachel Thomas. All rights reserved.
 //
 
