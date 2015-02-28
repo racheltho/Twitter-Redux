ef918a2cffa195e3eaac38a619089c9614f526da
Rachel Thomas <racheltho@gmail.com>
2015-02-27 16:09:41 -0800

    committing before I attempt to split timelineViewController into mentions and timeline options

--- Twitter-Redux/TwitterClient.swift
+++ Twitter-Redux/TwitterClient.swift
@@ -60,6 +60,18 @@ class TwitterClient: BDBOAuth1RequestOperationManager {
         }
     )}
 
+    func mentionsWithCompletion(params: NSDictionary?, completion: (tweets: [Tweet]?, error: NSError?) -> () ) {
+        GET("1.1/statuses/mentions_timeline.json", parameters: params, success: { (operation: AFHTTPRequestOperation!, response: AnyObject!) -> Void in
+            println("inside Timeline with Completion")
+            var tweets = Tweet.tweetsWithArray(response as [NSDictionary])
+            completion(tweets: tweets, error: nil)
+            }, failure: { (operation: AFHTTPRequestOperation!, error: NSError!) -> Void in
+                println("error getting home timeline")
+                completion(tweets: nil, error: error)
+        })
+        
+    }
+    
     func timelineWithCompletion(params: NSDictionary?, completion: (tweets: [Tweet]?, error: NSError?) -> () ) {
         GET("1.1/statuses/home_timeline.json", parameters: params, success: { (operation: AFHTTPRequestOperation!, response: AnyObject!) -> Void in
             println("inside Timeline with Completion")
